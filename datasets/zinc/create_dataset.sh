f8ff320f15816b135ac21fd7b18d61e34d0d735d
Aaron Vose <avose@aaronvose.net>
2020-04-04 12:45:56 -0400

    Working on database makefiles, updating zinc create script to make.

--- datasets/zinc/create_dataset.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -l
-
-set -e
-
-source ../../tools/enable_env_ubuntu.sh
-curl -o zinc15.csv 'http://zinc15.docking.org/substances.csv?count=all'
-
-python csv_to_dataset.py --csv zinc15.csv --out ./data
